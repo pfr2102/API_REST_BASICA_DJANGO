e41fb2eacfa0a365d1a8a2bd29a69cc0b2a9933b
pfr2102 <pfrs2102@gmail.com>
2024-04-12 20:35:51 -0700

    update readme

--- users/models.py
+++ users/models.py
@@ -7,5 +7,6 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     group = models.ForeignKey(Group, related_name='custom_user_groups', on_delete=models.SET_NULL, null=True)
 
+    #comenta estas lineas cuando creas un superusuario en la terminal
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
